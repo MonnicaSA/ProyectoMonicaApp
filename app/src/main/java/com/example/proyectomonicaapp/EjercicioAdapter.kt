269230ba78f6fa74d980139040f44551f5ebaf7d
Monica <jasmin.salinas584@gmail.com>
2024-02-02 10:29:06 +0100

    revisar activity formulario con el profe añadidas algunas clases mas

---
+++ app/src/main/java/com/example/proyectomonicaapp/EjercicioAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.proyectomonicaapp
+
+class EjercicioAdapter {
+}
---
+++ app/src/main/java/com/example/proyectomonicaapp/model/Ejercicios.kt
@@ -0,0 +1,8 @@
+package com.example.proyectomonicaapp.model
+
+data class Ejercicios(
+    val name: String,
+    val time: String,
+    val description: String,
+    val espacio: String
+)
